ababdffe890eddccec647f7c9fd12b0a78a8a4d9
AlexPopov <alexej_popov_88@mail.ru>
2020-07-24 17:02:18 +0300

    HomeWork3

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 "use strict";
-let numberOfFilms = 0;
-numberOfFilms = prompt('How many films have you watched ?', 0);
+let numberOfFilms;
+
+function start(){
+	numberOfFilms = +prompt('How many films have you watched ?', '');
+	while( numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) ) {
+		numberOfFilms = +prompt('How many films have you watched ?', '');
+	}
+}
+
+start();
 
 let personalMovieDB = {
   count : numberOfFilms,
@@ -12,52 +20,73 @@ let personalMovieDB = {
 
 let movie = '';
 
-
-for( let i = 0; i < 2; i++){
-	if (movie == '') {
-		movie = prompt('Last film you saw ?', 'Titanic');
-		if( movie == '' || movie.length > 50 || movie == null){
-			alert('Film name must be filled! Name length must be < 50!');
+function rememberMyFilms(){
+	for( let i = 0; i < 2; i++){
+		if (movie == '') {
+			movie = prompt('Last film you saw ?', 'Titanic');
+			if( movie == '' || movie.length > 50 || movie == null){
+				alert('Film name must be filled! Name length must be < 50!');
+				i--;
+				continue;
+			}
+		}
+		personalMovieDB.movies[movie] = prompt('What raiting did you get ?', 1.0);
+		if( personalMovieDB.movies[movie] == '' || personalMovieDB.movies[movie] == null){
+			alert( 'Film raiting must be filled!' );
 			i--;
 			continue;
 		}
+		movie = '';
 	}
-	personalMovieDB.movies[movie] = prompt('What raiting did you get ?', 1.0);
-	if( personalMovieDB.movies[movie] == '' || personalMovieDB.movies[movie] == null){
-		alert( 'Film raiting must be filled!' );
-		i--;
-		continue;
+	
+	// let j = 1;
+	// do{
+	// 	if (movie === '') {
+	// 		movie = prompt('Last film you saw ?', 'Titanic');
+	// 		if( movie === '' || movie.length > 50 || movie == null){
+	// 			alert('Film name must be filled! Name length must be < 50!');
+	// 			continue;
+	// 		}
+	// 	}
+	// 	personalMovieDB.movies[movie] = prompt('What raiting did you get ?', 1.0);
+	// 	if( personalMovieDB.movies[movie] === '' || personalMovieDB.movies[movie] == null){
+	// 		alert( 'Film raiting must be filled!' );
+	// 		continue;
+	// 	}
+	// 	j++;
+	// 	movie = '';
+	// } while( j < 3);
+}
+rememberMyFilms();
+
+function detectPersonalLevel(){
+	if(personalMovieDB.count < 11) {
+		alert('Просмотрено довольно мало фильмов');
+	} else if(personalMovieDB.count < 31 ){
+		alert('Вы классический зритель');
+	} else if(personalMovieDB.count > 30) {
+		alert('Вы киноман');
+	} else{
+		alert('Произошла ошибка');
 	}
-	movie = '';
 }
 
-// let j = 1;
-// do{
-// 	if (movie === '') {
-// 		movie = prompt('Last film you saw ?', 'Titanic');
-// 		if( movie === '' || movie.length > 50 || movie == null){
-// 			alert('Film name must be filled! Name length must be < 50!');
-// 			continue;
-// 		}
-// 	}
-// 	personalMovieDB.movies[movie] = prompt('What raiting did you get ?', 1.0);
-// 	if( personalMovieDB.movies[movie] === '' || personalMovieDB.movies[movie] == null){
-// 		alert( 'Film raiting must be filled!' );
-// 		continue;
-// 	}
-// 	j++;
-// 	movie = '';
-// } while( j < 3);
-
-if(personalMovieDB.count < 11) {
-	alert('Просмотрено довольно мало фильмов');
-} else if(personalMovieDB.count < 31 ){
-	alert('Вы классический зритель');
-} else if(personalMovieDB.count > 30) {
-	alert('Вы киноман');
-} else{
-	alert('Произошла ошибка');
+function showMyDB(){
+	if (personalMovieDB.privat == false) {
+		console.log(personalMovieDB);
+		console.log(personalMovieDB.movies);
+	}
 }
 
-console.log(personalMovieDB);
-console.log(personalMovieDB.movies);
+showMyDB();
+
+function writeYourGenres(){
+	let geners = [];
+	for (let index = 1; index < 4; index++) {
+		geners[index - 1] = prompt(`Ваш любимый жанр под номером ${index}`, 1.0);
+
+	}
+	console.log(geners);
+}
+
+writeYourGenres();
