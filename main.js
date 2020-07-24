2302e003c7616739c0dac2bad16652127844443a
AlexPopov <alexej_popov_88@mail.ru>
2020-07-24 15:37:06 +0300

    HomeWork2

--- main.js
+++ main.js
@@ -9,13 +9,55 @@ let personalMovieDB = {
   genres : [],
   privat : false
 };
- let tmp = 4;
-let movie = '';
-movie = prompt('Last film you saw ?', 'Titanic');
-personalMovieDB.movies[movie] = prompt('What raiting did you get ?', 1.0);
 
-movie = prompt('Last film you saw ?', 'Titanic');
-personalMovieDB.movies[movie] = prompt('What raiting did you get ?', 1.0);
+let movie = '';
+
+
+for( let i = 0; i < 2; i++){
+	if (movie == '') {
+		movie = prompt('Last film you saw ?', 'Titanic');
+		if( movie == '' || movie.length > 50 || movie == null){
+			alert('Film name must be filled! Name length must be < 50!');
+			i--;
+			continue;
+		}
+	}
+	personalMovieDB.movies[movie] = prompt('What raiting did you get ?', 1.0);
+	if( personalMovieDB.movies[movie] == '' || personalMovieDB.movies[movie] == null){
+		alert( 'Film raiting must be filled!' );
+		i--;
+		continue;
+	}
+	movie = '';
+}
+
+// let j = 1;
+// do{
+// 	if (movie === '') {
+// 		movie = prompt('Last film you saw ?', 'Titanic');
+// 		if( movie === '' || movie.length > 50 || movie == null){
+// 			alert('Film name must be filled! Name length must be < 50!');
+// 			continue;
+// 		}
+// 	}
+// 	personalMovieDB.movies[movie] = prompt('What raiting did you get ?', 1.0);
+// 	if( personalMovieDB.movies[movie] === '' || personalMovieDB.movies[movie] == null){
+// 		alert( 'Film raiting must be filled!' );
+// 		continue;
+// 	}
+// 	j++;
+// 	movie = '';
+// } while( j < 3);
+
+if(personalMovieDB.count < 11) {
+	alert('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count < 31 ){
+	alert('Вы классический зритель');
+} else if(personalMovieDB.count > 30) {
+	alert('Вы киноман');
+} else{
+	alert('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
 console.log(personalMovieDB.movies);
